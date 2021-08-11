d73514ae64b64d2bfae273304bc22fdcb8760c11
Ayush Mohanty <ayushmohantyrimon26@gmail.com>
2021-08-11 11:02:49 +0530

    init

---
+++ index.js
@@ -0,0 +1,37 @@
+let display= document.getElementById('display');
+let buttons = document.querySelectorAll('button');
+let screenValue= '';
+for(item of buttons){
+    item.addEventListener('click', (e)=>{
+        buttonText=e.target.innerText;
+        console.log("the number is", buttonText);
+
+        if(buttonText=="x")
+        {
+            buttonText ="*";
+            screenValue += buttonText;
+            display.value = screenValue;
+
+        }
+
+        else if(buttonText=="C"){
+            screenValue = "";
+            display.value = screenValue;
+        }
+
+        else if( buttonText=="="){
+            display.value= eval(screenValue);
+        }
+
+        else if( buttonText =="Del"){
+            display.value= screenValue.length-1;
+        }
+
+
+        else{
+            screenValue += buttonText;
+            display.value = screenValue;
+        }
+
+    })
+}
